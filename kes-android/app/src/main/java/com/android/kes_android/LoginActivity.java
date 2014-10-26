07a7535d9cd430e9d6c99be0ff23119b4a939506
Muaz Rahman <muazra@gmail.com>
2014-10-26 17:13:57 -0400

    mobile register account

--- kes-android/app/src/main/java/com/android/kes_android/LoginActivity.java
+++ kes-android/app/src/main/java/com/android/kes_android/LoginActivity.java
@@ -6,12 +6,12 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MainActivity extends Activity {
+public class LoginActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 
 
---
+++ kes-android/app/src/main/java/com/android/kes_android/RegisterActivity.java
@@ -0,0 +1,7 @@
+package com.android.kes_android;
+
+/**
+ * Created by Muaz on 10/26/14.
+ */
+public class RegisterActivity {
+}

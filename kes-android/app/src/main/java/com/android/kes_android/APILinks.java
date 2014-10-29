8b340ffc015faaadac9ba699fc244e78ae475b95
Muaz Rahman <muazra@gmail.com>
2014-10-28 23:21:22 -0400

    android capture image and send to server

--- kes-android/app/src/main/java/com/android/kes_android/APILinks.java
+++ kes-android/app/src/main/java/com/android/kes_android/APILinks.java
@@ -7,5 +7,6 @@ public class APILinks {
     public static final String USERS_URL = BASE_URL + "/api/users";
     public static final String GUESTS_URL = BASE_URL + "/api/guests";
     public static final String PHOTOS_URL = BASE_URL + "/static/";
+    public static final String TOGGLE_URL = BASE_URL + "/toggle/";
 
 }
--- kes-android/app/src/main/java/com/android/kes_android/DashActivity.java
+++ kes-android/app/src/main/java/com/android/kes_android/DashActivity.java
@@ -1,29 +1,132 @@
 package com.android.kes_android;
 
+import android.app.AlertDialog;
 import android.app.ListActivity;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Base64;
 import android.view.KeyEvent;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
-import java.util.ArrayList;
-import java.util.List;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.TextHttpResponseHandler;
+
+import org.apache.http.Header;
+
+import java.io.ByteArrayOutputStream;
 
 public class DashActivity extends ListActivity {
+
+    private Button mToggle;
+    private Context mContext = this;
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dash);
 
-        List<String> mDoorActivity = new ArrayList<String>();
-
-        ModelListAdapter adapter = new ModelListAdapter(this, mDoorActivity);
-        setListAdapter(adapter);
+        mToggle = (Button) findViewById(R.id.dash_toggle);
+        mToggle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
+        });
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            toggleDialog(imageBitmap).show();
+        }
+    }
+
+    private AlertDialog.Builder toggleDialog(final Bitmap imageBitmap){
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this, AlertDialog.THEME_HOLO_DARK);
+        LayoutInflater inflater = getLayoutInflater();
+        View customDialog = inflater.inflate(R.layout.dialog_toggle, null);
+
+        ImageView togglePhoto = (ImageView) customDialog.findViewById(R.id.dialog_toggle_image);
+        togglePhoto.setImageBitmap(imageBitmap);
+
+        builder.setView(customDialog);
+        builder.setTitle("CONFIRM IMAGE");
+
+        builder.setPositiveButton("SEND", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                //convert image to base64 and send to server
+                ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+                imageBitmap.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream);
+                byte[] byteArray = byteArrayOutputStream .toByteArray();
+                String encoded = Base64.encodeToString(byteArray, Base64.DEFAULT);
+                sendtoserver(encoded);
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                //do nothing
+            }
+        });
+        return builder;
+    }
+
+    private void sendtoserver(String encodedstring){
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get(APILinks.GUESTS_URL + encodedstring, new TextHttpResponseHandler() {
+            ProgressDialog mProgressDialog;
+
+            @Override
+            public void onStart(){
+                mProgressDialog = new ProgressDialog(mContext, ProgressDialog.THEME_HOLO_DARK);
+                mProgressDialog.setTitle("Sending");
+                mProgressDialog.setMessage("Please wait..");
+                mProgressDialog.show();
+            }
+
+            @Override
+            public void onSuccess(int i, Header[] headers, String s) {
+                mProgressDialog.dismiss();
+                if(s.equals("success")) {
+                    Toast.makeText(getApplicationContext(), "Door successfully toggled",
+                            Toast.LENGTH_LONG).show();
+                }else {
+                    Toast.makeText(getApplicationContext(), "Door toggle request denied",
+                            Toast.LENGTH_LONG).show();
+                }
+            }
+
+            @Override
+            public void onFinish(){
+                mProgressDialog.dismiss();
+
+            }
+
+            @Override
+            public void onFailure(int i, Header[] headers, String s, Throwable throwable) {
+
+            }
+
+        });
+    }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
